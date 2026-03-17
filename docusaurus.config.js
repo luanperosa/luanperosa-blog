818d66231d5f4db1b7d203d7e55016b0439cd653
luanperosa <luan.perosa@gmail.com>
2026-03-17 00:31:54 -0300

    chore: update baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,12 @@ const config = {
   url: 'https://luanperosa.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/luanperosa-blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'luanperosa', // Usually your GitHub org/user name.
   projectName: 'luanperosa-blog', // Usually your repo name.
-  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
